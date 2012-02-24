f3d102e1b47b7ce4edf6cb6829f7c585e3c351f4
Petre Andrei <p31andrei (at) gmail.com>
2012-02-25 00:51:21 +0200

    [BROKEN] do_turn should make ants go on food paths.

--- RandomBot.py
+++ RandomBot.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 from random import shuffle
 from ants import *
-from math import abs
 from heapq import heappush, heappop
 
 class RandomBot:
+    def __init__(self):
+        self.paths = {}         # paths for ants
+
     def heuristic_cost_estimate(self, start, goal):
         '''Heuristic give estimated cost to goal'''
-        return abs(start[0] - goal[0]) + abs(start[1] - goal[1])
+        return ants.distance(start[0], start[1], goal[0], goal[1])
 
     def neighbor_nodes(self, current):
         '''Return all neighbors for a given node'''
@@ -26,8 +28,14 @@ class RandomBot:
             current_node = came_from[current_node]
         return path
 
-    def Astar(self, start, goal):
-        '''start, goal are tuples of the form (row, col)'''
+    def Astar(self, start, goal, obstacles):
+        '''
+        start, goal are tuples of the form (row, col);
+        obstacles is a function : (row, col) -> return Bool
+        '''
+        if (obstacles(start[0], start[1]) or obstacles(goal[0], goal[1])):
+            return None
+
         closedset = {}      # The set of nodes already evaluated.
         openset = [start]   # The set of tentative nodes, to be evaluated.
         came_from = {}      # The map of navigated nodes.
@@ -46,7 +54,8 @@ class RandomBot:
                 return reconstruct_path(came_from, goal)
             closedset[current] = True        # True that current is in dict
             for neighbor in neighbor_nodes(current):
-                if closedset.__contains__(neighbor):
+                if (obstacles(current[0], current[1]) or
+                closedset.__contains__(neighbor)):
                     continue
                 if neighbor not in openset:
                     openset += [neighbor]
@@ -60,18 +69,33 @@ class RandomBot:
     def do_turn(self, ants):
         destinations = []
         for a_row, a_col in ants.my_ants():
-            # try all directions randomly until one is passable and not occupied
-            directions = AIM.keys()
-            shuffle(directions)
-            for direction in directions:
-                (n_row, n_col) = ants.destination(a_row, a_col, direction)
-                if (not (n_row, n_col) in destinations and
-                        ants.passable(n_row, n_col)):
-                    ants.issue_order((a_row, a_col, direction))
-                    destinations.append((n_row, n_col))
-                    break
+            # If ant has a path to follow, follow it.
+            if paths.__contains__((a_row, a_col)):
+                path = paths.pop((a_row, a_col))    # Get path of this ant
             else:
-                destinations.append((a_row, a_col))
+                closest_food = ants.closest_food(a_row, a_col)
+                if closest_food != None:
+                    path = Astar((a_row, a_col), closest_food, ants.passable)
+                # Try all directions randomly until one is passable and not
+                # occupied.
+                else:
+                    directions = AIM.keys()
+                    shuffle(directions)
+                    for direction in directions:
+                        (n_row, n_col) = ants.destination(a_row, a_col, direction)
+                        if (not (n_row, n_col) in destinations and
+                            ants.passable(n_row, n_col)):
+                            ants.issue_order((a_row, a_col, direction))
+                            destinations.append((n_row, n_col))
+                            break
+                    else:
+                        destinations.append((a_row, a_col))
+                    continue
+            (n_row, n_col) = path.pop(0)        # Get next move.
+            direction = ants.direction(a_row, a_col, n_row, n_col)
+            paths[(n_row, n_col)] = path;       # Update dict of paths.
+            ants.issue_order((n_row, n_col, direction))
+            destinations.append((n_row, n_col))
 
 if __name__ == '__main__':
     try:
@@ -83,3 +107,4 @@ if __name__ == '__main__':
         Ants.run(RandomBot())
     except KeyboardInterrupt:
         print('ctrl-c, leaving ...')
+        k 
