965bc0aeb1b85e7ec16af5692e4a1f5a80da0e9c
Petre Andrei <p31andrei (at) gmail.com>
2012-02-24 22:08:41 +0200

    Testing A* algorithm for food gathering.

--- RandomBot.py
+++ RandomBot.py
@@ -1,8 +1,62 @@
 #!/usr/bin/env python
 from random import shuffle
 from ants import *
+from math import abs
+from heapq import heappush, heappop
 
 class RandomBot:
+    def heuristic_cost_estimate(self, start, goal):
+        '''Heuristic give estimated cost to goal'''
+        return abs(start[0] - goal[0]) + abs(start[1] - goal[1])
+
+    def neighbor_nodes(self, current):
+        '''Return all neighbors for a given node'''
+        l = []
+        l += (current[0] + 1, current[1])
+        l += (current[0] - 1, current[1])
+        l += (current[0], current[1] + 1)
+        l += (current[0], current[1] - 1)
+        return l
+
+    def reconstruct_path(came_from, current_node):
+        '''Construct path to target, from the beginning'''
+        path = []
+        while current_node != None:
+            path.insert(0, current_node)
+            current_node = came_from[current_node]
+        return path
+
+    def Astar(self, start, goal):
+        '''start, goal are tuples of the form (row, col)'''
+        closedset = {}      # The set of nodes already evaluated.
+        openset = [start]   # The set of tentative nodes, to be evaluated.
+        came_from = {}      # The map of navigated nodes.
+        heap = []
+
+        # Stats for starting node.
+        g_score = 0
+        h_score = heuristic_cost_estimate(start, goal)
+        f_score = g_score + h_score
+        came_from[start] = None
+        heappush(heap, (f_score, start))    # Use heap to store by f_score.
+
+        while openset != []:
+            _, current = heappop(heap)      # (f_score, current)
+            if current == goal:
+                return reconstruct_path(came_from, goal)
+            closedset[current] = True        # True that current is in dict
+            for neighbor in neighbor_nodes(current):
+                if closedset.__contains__(neighbor):
+                    continue
+                if neighbor not in openset:
+                    openset += [neighbor]
+                    h_score = heuristic_cost_estimate(neighbor, goal)
+                    g_score = heuristic_cost_estimate(start, neighbor)
+                    f_score = g_score + h_score
+                    heappush(heap, (f_score, start))
+                    came_from[neighbor] = current
+        return None
+
     def do_turn(self, ants):
         destinations = []
         for a_row, a_col in ants.my_ants():
