c4d9373bbcbe0e63f6bce3ecb6fc0c413ce10652
Mihai Barbulescu <b12mihai@gmail.com>
2012-02-25 14:40:03 +0200

    Created logging to logFile.log

--- RandomBot.py
+++ RandomBot.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python
 from random import shuffle
 from ants import *
+from logging import *
+from logutils import *
 
 class RandomBot:
+
+    def __init__(self):
+	# for logging
+        self.logger = logging.getLogger('myapp')
+        hdlr = logging.FileHandler('logFile.log')
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        hdlr.setFormatter(formatter)
+        self.logger.addHandler(hdlr) 
+        self.logger.setLevel(logging.INFO)
+    
     def do_turn(self, ants):
+
+        self.logger.info("Strating game")
         destinations = []
         for a_row, a_col in ants.my_ants():
             # try all directions randomly until one is passable and not occupied
             directions = AIM.keys()
+            self.logger.info("I am loooooping, coaieeeee")
             shuffle(directions)
             for direction in directions:
+                self.logger.info("Ana are mere, pere si prune")
                 (n_row, n_col) = ants.destination(a_row, a_col, direction)
                 if (not (n_row, n_col) in destinations and
                         ants.passable(n_row, n_col)):
