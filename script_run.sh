f3de11ca780b3c74661ca78bbe161d3f5c7db9fc
Mihai Barbulescu <b12mihai@gmail.com>
2012-03-04 14:52:22 +0200

    Update README and script_run.sh

--- script_run.sh
+++ script_run.sh
@@ -1,18 +1,21 @@
-#run our bot with adversar
+#Script de testare a modului in care 
+#lupta botul nostru in prima etapa
+#cu adversarii
 
-# path to your lin directory
+# Calea catre fisierele din tools_lin.zip
+# descarcate de pe cs.curs.pub.ro
 lin=lin
-# all players must be in this directory
-player1="MyBot.py"		# our bot
-player2="./opponent"			# our opponent
-map="$lin/maps/cell_maze/cell_maze_p02_19.map"	# running map
-map1="$lin/maps/cell_maze/cell_maze_p02_20.map"	# running map
-map2="$lin/maps/random_walk/random_walk_p02_21.map"	# running map
-map3="$lin/maps/random_walk/random_walk_p02_24.map"	# running map
+# Presupunere: toti jucatorii se afla in directorul curent
+player1="MyBot.py"
+player2="./opponent"
+map="$lin/maps/cell_maze/cell_maze_p02_19.map"
+map1="$lin/maps/cell_maze/cell_maze_p02_20.map"
+map2="$lin/maps/random_walk/random_walk_p02_21.map"
+map3="$lin/maps/random_walk/random_walk_p02_24.map"
 
 
-chmod +x $player1
-chmod +x $player2
+chmod a+x $player1
+chmod a+x $player2
 
 ./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map "python $player1" $player2|java -jar $lin/visualizer.jar
 ./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map1 "python $player1" $player2|java -jar $lin/visualizer.jar
