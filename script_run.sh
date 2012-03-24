324e520ac8ab826f99df6a74ec461e29bc43369f
Traista-Popescu Vlad <vladtp91@gmail.com>
2012-03-24 14:18:28 +0200

    Modified script_run

--- script_run.sh
+++ script_run.sh
@@ -6,8 +6,8 @@
 # descarcate de pe cs.curs.pub.ro
 lin=lin
 # Presupunere: toti jucatorii se afla in directorul curent
-player1="MyBot.py"
-player2="./bot32"
+player1="./bot32"
+player2="MyBot.py"
 map="$lin/maps/cell_maze/cell_maze_p02_19.map"
 map1="$lin/maps/cell_maze/cell_maze_p02_20.map"
 map2="$lin/maps/random_walk/random_walk_p02_21.map"
@@ -17,7 +17,7 @@ map3="$lin/maps/random_walk/random_walk_p02_24.map"
 chmod a+x $player1
 chmod a+x $player2
 
-./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map "python $player1" $player2|java -jar $lin/visualizer.jar
-./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map1 "python $player1" $player2|java -jar $lin/visualizer.jar
-./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map2 "python $player1" $player2|java -jar $lin/visualizer.jar
-./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map3 "python $player1" $player2|java -jar $lin/visualizer.jar
+./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map $player1 "python $player2"|java -jar $lin/visualizer.jar
+./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map1 $player1 "python $player2"|java -jar $lin/visualizer.jar
+./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map2 $player1 "python $player2"|java -jar $lin/visualizer.jar
+./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map3 $player1 "python $player2"|java -jar $lin/visualizer.jar
