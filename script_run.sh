c1514090f4edd35acbfdb97c47f1c63e0718742d
Mihai Barbulescu <b12mihai@gmail.com>
2012-04-21 20:08:14 +0300

    Adaugat bot pentru etapa 3. Script mai destept, se ruleaza cate o harta sau la cerere toate

--- script_run.sh
+++ script_run.sh
@@ -5,19 +5,39 @@
 # Calea catre fisierele din tools_lin.zip
 # descarcate de pe cs.curs.pub.ro
 lin=lin
+
 # Presupunere: toti jucatorii se afla in directorul curent
 player1="./bot32"
 player2="MyBot.py"
-map="$lin/maps/cell_maze/cell_maze_p02_19.map"
-map1="$lin/maps/cell_maze/cell_maze_p02_20.map"
-map2="$lin/maps/random_walk/random_walk_p02_21.map"
-map3="$lin/maps/random_walk/random_walk_p02_24.map"
 
+map="$lin/maps/random_walk/random_walk_p02_10.map"
+map1="$lin/maps/cell_maze/cell_maze_p02_10.map"
+map2="$lin/maps/cell_maze/cell_maze_p02_15.map"
+map3="$lin/maps/maze/maze_p02_14.map"
 
 chmod a+x $player1
 chmod a+x $player2
 
-./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map $player1 "python $player2"|java -jar $lin/visualizer.jar
+case $1 in
+map=1)
+	echo "Running map: $map"
+	./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 1000 --map_file $map $player1 "python $player2"|java -jar $lin/visualizer.jar;;
+map=2)
+	echo "Running map $map1"
+	./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 1000 --map_file $map1 $player1 "python $player2"|java -jar $lin/visualizer.jar;;
+map=3)
+	echo "Running map $map2"
+	./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 1000 --map_file $map2 $player1 "python $player2"|java -jar $lin/visualizer.jar;;
+map=4)
+	echo "Running map $map3"
+	./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 1000 --map_file $map3 $player1 "python $player2"|java -jar $lin/visualizer.jar;;
+map=all)
+	echo "Running all maps. Please be patient"
+	./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map $player1 "python $player2"|java -jar $lin/visualizer.jar
 ./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map1 $player1 "python $player2"|java -jar $lin/visualizer.jar
 ./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map2 $player1 "python $player2"|java -jar $lin/visualizer.jar
 ./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map3 $player1 "python $player2"|java -jar $lin/visualizer.jar
+;;
+*)
+	echo "Mai mult de 4 harti nu exista";;
+esac
