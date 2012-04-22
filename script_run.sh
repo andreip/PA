e361844714ec56a974711e2265191f2d71818cf1
Iulian Stana <iulian@NucaZOoOo.(none)>
2012-04-22 18:31:57 +0300

    Fara crash dar cu time out tot Astar devina.

--- script_run.sh
+++ script_run.sh
@@ -8,16 +8,16 @@ lin=lin
 # Presupunere: toti jucatorii se afla in directorul curent
 player1="./bot32"
 player2="MyBot.py"
-map="$lin/maps/cell_maze/cell_maze_p02_19.map"
-map1="$lin/maps/cell_maze/cell_maze_p02_20.map"
-map2="$lin/maps/random_walk/random_walk_p02_21.map"
+map="$lin/maps/cell_maze/cell_maze_p02_10.map"
+map1="$lin/maps/cell_maze/cell_maze_p02_15.map"
+map2="$lin/maps/random_walk/random_walk_p02_10.map"
 map3="$lin/maps/random_walk/random_walk_p02_24.map"
 
 
 chmod a+x $player1
 chmod a+x $player2
 
-./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map $player1 "python $player2"|java -jar $lin/visualizer.jar
-./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map1 $player1 "python $player2"|java -jar $lin/visualizer.jar
-./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map2 $player1 "python $player2"|java -jar $lin/visualizer.jar
-./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map3 $player1 "python $player2"|java -jar $lin/visualizer.jar
+#./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map $player1 "python $player2"|java -jar $lin/visualizer.jar
+#./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map1 $player1 "python $player2"|java -jar $lin/visualizer.jar
+./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 1000 --map_file $map2 $player1 "python $player2"|java -jar $lin/visualizer.jar
+#./$lin/playgame.py --log_dir $lin/game_logs -So --verbose --engine_seed 42 --player_seed 0 --turns 100 --map_file $map3 $player1 "python $player2"|java -jar $lin/visualizer.jar
